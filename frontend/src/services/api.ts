0e8f159098bd20efdcdc3cb8290fa21cb54cf2ee
Josh Fouchey <joshfouchey@gmail.com>
2025-10-25 11:18:08 -0400

    config for urls

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_BASE = (import.meta.env.VITE_API_BASE as string) || '';
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8080";
+
 /* ==============================
    Types / Interfaces
 ============================== */
